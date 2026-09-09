21a384b9a880cdfff4e094c3816f92c8accecf26
modebrecht <137332207+modebrecht@users.noreply.github.com>
2026-09-09 21:49:05 +0200

    Load Studio 2 and Studio 3 interior rebuild

--- src/components/cover/fresh-templates.ts
+++ src/components/cover/fresh-templates.ts
@@ -16,6 +16,7 @@ import "./next-signature-templates.css";
 import "./verlauf-pill-fix.css";
 import "./templatefix-24-25.css";
 import "./templatefix-violet-pulse.css";
+import "./templatefix-27-28.css";
 
 export { FRESH_TEMPLATE_IDS, type FreshTemplateId } from "./fresh-template-registry";
 
