1cb7263b2db5e81ebe646e4796872325fb6f847d
modebrecht <137332207+modebrecht@users.noreply.github.com>
2026-09-09 21:54:41 +0200

    Load Warm and Ledger template fixes

--- src/components/cover/fresh-templates.ts
+++ src/components/cover/fresh-templates.ts
@@ -17,6 +17,7 @@ import "./verlauf-pill-fix.css";
 import "./templatefix-24-25.css";
 import "./templatefix-violet-pulse.css";
 import "./templatefix-27-28.css";
+import "./templatefix-29-31.css";
 
 export { FRESH_TEMPLATE_IDS, type FreshTemplateId } from "./fresh-template-registry";
 
