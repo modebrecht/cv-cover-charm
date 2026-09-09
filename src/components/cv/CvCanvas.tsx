abe1df76eb5d600273b75535799c7eb1ad665bfb
modebrecht <137332207+modebrecht@users.noreply.github.com>
2026-09-09 12:08:16 +0200

    Load Blockig sidebar polish

--- src/components/cv/CvCanvas.tsx
+++ src/components/cv/CvCanvas.tsx
@@ -7,6 +7,7 @@ import {
 import { CvCanvas as BaseCvCanvas } from "./CvCanvasBase";
 import type { CvData, CvDesign } from "./types";
 import "@/components/dossier/edel-stationery.css";
+import "./blockig-sidebar-polish.css";
 import "./full-section-rules.css";
 import "./user-typography.css";
 
